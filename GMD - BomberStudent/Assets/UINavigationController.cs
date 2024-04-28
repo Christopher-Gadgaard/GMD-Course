a9280505aec6c1a2c18a21c2fff2df53bf7e1bcb
Chris309283 <Christopher.holmelund@gmail.com>
2024-04-29 01:37:28 +0200

    started adding some game UI

--- GMD - BomberStudent/Assets/UINavigationController.cs
+++ GMD - BomberStudent/Assets/UINavigationController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UINavigationController : MonoBehaviour
 {
@@ -13,22 +14,25 @@ public class UINavigationController : MonoBehaviour
     private Button currentSelectedButton;
     private Button[] buttons;
     private int currentIndex;
-    void Start()
+
+    private void Start()
     {
         root = uiDocument.rootVisualElement;
         singleplayerButton = root.Q<Button>("Singleplayer");
         multiplayerButton = root.Q<Button>("Multiplayer");
         highScoresButton = root.Q<Button>("HighScore");
-        
+
         buttons = new[] { singleplayerButton, multiplayerButton, highScoresButton };
 
-        SetSelectedButton(buttons[currentIndex]); 
+        SetSelectedButton(buttons[currentIndex]);
     }
+
     private void Awake()
     {
         controls = new MainMenuControls();
         controls.MainMenu.MoveUp.performed += _ => MoveSelection(-1);
         controls.MainMenu.MoveDown.performed += _ => MoveSelection(1);
+        controls.MainMenu.Select.performed += _ => CurrentButtonAction(); 
     }
 
     private void OnEnable()
@@ -43,27 +47,37 @@ public class UINavigationController : MonoBehaviour
 
     private void SetSelectedButton(Button button)
     {
-        if (currentSelectedButton != null)
-        {
-            // Remove 'selected' class from the previously selected button
-            currentSelectedButton.RemoveFromClassList("current");
-        }
-
-        // Set the new button as the selected one and update the class list
+        currentSelectedButton?.RemoveFromClassList("current");
         currentSelectedButton = button;
         currentSelectedButton.AddToClassList("current");
-
-        // You can also set the focus to the current button if needed
-        // This will ensure that if any key is pressed, the currently selected button is activated
         currentSelectedButton.Focus();
     }
 
-    void MoveSelection(int direction)
+    private void MoveSelection(int direction)
     {
-        // Calculate the new index using modular arithmetic to wrap around the button array
         currentIndex = (currentIndex + direction + buttons.Length) % buttons.Length;
-
-        // Set the new selected button using the new index
         SetSelectedButton(buttons[currentIndex]);
     }
+
+    private void CurrentButtonAction()
+    {
+        if (currentSelectedButton == singleplayerButton)
+        { 
+            LoadScene("SingleplayerScene");
+        }
+        else if (currentSelectedButton == multiplayerButton)
+        {
+            LoadScene("MultiplayerScene");
+        }
+        else if (currentSelectedButton == highScoresButton)
+        {
+            LoadScene("HighScoresScene");
+        }
+    }
+    
+    private static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
+
